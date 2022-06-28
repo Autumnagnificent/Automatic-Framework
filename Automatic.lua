e656dfdb0eec0affa0130fdc83d266849913404c
Austin <barnes06.austin@gmail.com>
2022-06-28 13:44:50 -0700

    Testing

--- Automatic.lua
+++ Automatic.lua
@@ -1,4 +1,5 @@
 Auto = {}
+AutoUI = {}
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------
 ----------------Arithmetic-----------------------------------------------------------------------------------------------------------------------------
@@ -11,7 +12,7 @@ Auto = {}
 ---@param steep number
 ---@param offset number
 ---@return number
-function Auto.logistic(v, max, steep, offset)
+function AutoUI.logistic(v, max, steep, offset)
 	return max / (1 + math.exp((v - offset) * steep))
 end
 
@@ -20,14 +21,14 @@ end
 ---@param n number
 ---@param d number
 ---@return number
-function Auto.round(n,d)x=1/d return math.floor(n*x+.5)/x end
+function AutoUI.round(n,d)x=1/d return math.floor(n*x+.5)/x end
 
 ---Limits a value from going below the min and above the max
 ---@param val number
 ---@param min number
 ---@param max number
 ---@return number
-function Auto.clamp(val, min, max)
+function AutoUI.clamp(val, min, max)
 	if val < min then
 		return min
 	elseif val > max then
@@ -41,7 +42,7 @@ end
 ---@param a number
 ---@param b number
 ---@return number
-function Auto.big(a, b)
+function AutoUI.big(a, b)
 	if math.abs( a ) > math.abs( b ) then
 		return a
 	else
@@ -53,7 +54,7 @@ end
 ---@param a number
 ---@param b number
 ---@return number
-function Auto.small(a, b)
+function AutoUI.small(a, b)
 	if math.abs( a ) < math.abs( b ) then
 		return a
 	else
@@ -64,7 +65,7 @@ end
 ---Counts the amount of elements in a list
 ---@param t table
 ---@return integer
-function Auto.count(t)
+function AutoUI.count(t)
 	local c = 0
 	for i in pairs(t) do
 		c = c + 1
@@ -77,7 +78,7 @@ end
 ---@param length any
 ---@param precision any
 ---@return table
-function Auto.rndVec(length, precision)
+function AutoUI.rndVec(length, precision)
 	precision = precision or 0.01
 	local m = 1/precision
 	local v = VecNormalize(Vec(math.random(-m,m), math.random(-m,m), math.random(-m,m)))
@@ -89,7 +90,7 @@ end
 ---@param b number
 ---@param t number
 ---@return number
-function Auto.lerpUnclamped(a, b, t)
+function AutoUI.lerpUnclamped(a, b, t)
 	return (1-t)*a + t*b
 end
 
@@ -98,7 +99,7 @@ end
 ---@param b number
 ---@param t number
 ---@return number
-function Auto.move(a, b, t)
+function AutoUI.move(a, b, t)
 	output = a
     if a == b then
 		return a
@@ -117,7 +118,7 @@ end
 ---@param a Vec
 ---@param b Vec
 ---@return number
-function Auto.vecDist(a, b)
+function AutoUI.vecDist(a, b)
 	return math.sqrt( (a[1] - b[1])^2 + (a[2] - b[2])^2 + (a[3] - b[3])^2 )
 end
 
@@ -125,7 +126,7 @@ end
 ---@param a number
 ---@param b number
 ---@return number
-function Auto.dist(a, b)
+function AutoUI.dist(a, b)
 	return math.abs(a - b)
 end
 
@@ -133,58 +134,91 @@ end
 ----------------User Interface-------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local PresetPadding = {none = 0, thin = 4, thick = 16, heavy = 32}
-local AutoAlign = 'top' -- Can be : 'top', 'left', 'right', 'bottom'
+local AutoUIStack = {
+	
+}
+
+local PresetPadding = {none = 0, thin = 4, thick = 16, heavy = 32, beefy = 128}
+local Spacing = PresetPadding.thick
+local ExitAlign = 'top' -- Can be : 'top', 'left', 'right', 'bottom'. The default is 'top'
+
+function ToPad(pad)
+	local t = type(pad)
+	if t == "number" then
+		return t
+	elseif t == "string" then
+		return PresetPadding[pad]
+	elseif t == nil then
+		return PresetPadding.thick
+	end
+end
+
+function AutoUI.Push()
+	
+end
 
 ---Set the Automatic Alignment for Auto.Ui- functions. Used like UiAlign(). UiPush() and UiPop() have no effect.
----@param alignment string Can be : 'top', 'left', 'right', 'bottom'
-function Auto.SetAutoAlign(alignment) AutoAlign = alignment end
+---@param alignment string Can be : 'top', 'left', 'right', 'bottom'. The default is 'top'
+function AutoUI.SetExitAlign(alignment)
+	ExitAlign = alignment or 'top'
+end
+
+---Set the Automatic Spacing for Auto.Ui- functions. Used like padding. UiPush() and UiPop() have no effect.
+---@param spacing string Can be : 'none', 'thin', 'thick', 'heavy', 'beefy'. default is 'thick'
+function AutoUI.SetSpacing(spacing)
+	Spacing = ToPad(spacing)
+end
 
 ---UiTranslate() but for using padding values
 ---@param padding table Uses values from the PresetPadding, default is {"heavy", "heavy"}
 ---@param multiplyer number A multiplyer to the padding, default is 1
-function Auto.UiPad(padding, multiplyer)
-	padding = padding or {'heavy', 'heavy'}
-    padx = PresetPadding[padding[1]]
-    pady = PresetPadding[padding[2]]
+function AutoUI.UiPad(padding, multiplyer)
+	padding = padding or { 'thick', 'thick' }
+	padding = { w = ToPad(padding[1]), h = ToPad(padding[2]) }
     multiplyer = multiplyer or 1
     
-    UiTranslate(padx * multiplyer, pady * multiplyer)
+    UiTranslate(padding.w * multiplyer, padding.h * multiplyer)
 end
 
 ---The Main Component of the Ui part of the Automatic Module.
 ---@param width number The width of the Container, default is 300
 ---@param height number The height of the Container, default is 400
----@param align string Alignment
----@param padding table Adds Top padding based off of the PresetPadding, default is {"heavy", "heavy"}
+---@param alignment string Alignment
+---@param color table A table containg the red, green, blue, and alpha. default is {0, 0, 0, 0.55}
 ---@param name string Adds a title above the Container
+---@param style string Can choose from 'solid' or 'border', default is 'solid'
 ---@param animation number An optional value used to animate the window, simply increment the value from 0 to 1.
 ---@return data table
-function Auto.UiContainer(width, height, align, padding, name, animation)
-    align = align or 'left top'
-	padding = padding or { 'heavy', 'heavy' }
-	padding = { x = PresetPadding[ padding[1] ], y = PresetPadding[ padding[2] ] }
-	
-	animation = animation and Auto.logistic(animation, 1, -10, 0.5) or 1
+function AutoUI.UiContainer(width, height, alignment, color, name, style, animation)
     width = width or 300
 	height = (height or 400)
+    alignment = alignment or 'left top'
+	color = color or {0, 0, 0, 0.55}
+	style = style or 'solid'
+	animation = animation and Auto.logistic(animation, 1, -10, 0.5) or 1
+
 	height = height * (animation + (20 / height) * (1 - animation))
     
-    UiAlign(align)
-    UiColor(0, 0, 0, 0.55 * math.min(animation * 4, 1))
-    UiImageBox("ui/common/box-solid-10.png", width, height, 10, 10)
+    UiAlign(alignment)
+    UiColor(color[1], color[2], color[3], color[4] * math.min(animation * 4, 1))
+	if style == 'solid' then
+		UiImageBox("ui/common/box-solid-10.png", width, height, 10, 10)
+	else
+		UiImageBox("ui/common/box-outline-6.png", width, height, 6, 6)
+	end
+		
     UiWindow(width, height, true)
     hover = UiIsMouseInRect(width, height)
-    
+
     if name then
         UiPush()
-            if string.find(align, 'left') then
+            if string.find(alignment, 'left') then
                 UiAlign('left bottom')
                 UiTranslate(PresetPadding.thin, -PresetPadding.thin)
-            elseif string.find(align, 'center') then
+            elseif string.find(alignment, 'center') then
                 UiAlign('center bottom')
                 UiTranslate(UiCenter(), -PresetPadding.thin)
-            elseif string.find(align, 'right') then
+            elseif string.find(alignment, 'right') then
                 UiAlign('right bottom')
                 UiTranslate(UiWidth() - PresetPadding.thin, -PresetPadding.thin)
             end
@@ -196,17 +230,32 @@ function Auto.UiContainer(width, height, align, padding, name, animation)
         UiPop()
     end
     
-    UiAlign('center top')
-    UiTranslate(width / 2, padding.y)
+	if ExitAlign == 'top' then
+		UiAlign('center top')
+		UiTranslate(width / 2, Spacing)
+	elseif ExitAlign == 'bottom' then
+		UiAlign('center bottom')
+		UiTranslate(width / 2, height - Spacing)
+	elseif ExitAlign == 'left' then
+		UiAlign('left middle')
+		UiTranslate(Spacing, height / 2)
+	elseif ExitAlign == 'right' then
+		UiAlign('right middle')
+		UiTranslate(width - Spacing, height / 2)
+	end
 
     return {rect = {w = width, h = height}, hover = hover}
 end
 
-function Auto.UiEasyButton(name, padding, animation)
+---Creates a Button
+---@param name string The displayed text inside the Button
+---@param padding string The amount of padding between the text and the button border. default is {'thick', 'thick'}
+---@param animation number An optional value used to animate the button, simply increment the value from 0 to 1.
+---@return data table
+function AutoUI.UiButton(name, padding, animation)
 	animation = animation and Auto.logistic(animation, 1, -10, 0.5) or 1
-    padding = padding or {'heavy', 'heavy'}
-    padx = PresetPadding[padding[1]]
-    pady = PresetPadding[padding[2]]
+	padding = padding or { 'thick', 'thick' }
+	padding = { w = ToPad(padding[1]), h = ToPad(padding[2]) }
 
 	UiPush()
         UiPush()
@@ -214,55 +263,77 @@ function Auto.UiEasyButton(name, padding, animation)
             UiFont("regular.ttf", 28)
             
             UiColor(0,0,0,0)
-            local frw, frh = UiText(name)
-            UiTranslate(0,frh/2)
-            UiAlign('center middle')
+			UiAlign('center middle')
+            local rw, rh = UiText(name)
+			local prw, prh = (rw + padding.w), (rh + padding.h)
+			local hprw, hprh = (rw + padding.w/2), (rh + padding.h/2)
+			if ExitAlign == 'top' then
+				UiTranslate(0,hprh)
+			elseif ExitAlign == 'bottom' then
+				UiTranslate(0,-hprh)
+			elseif ExitAlign == 'left' then
+				UiTranslate(rw,-hprh)
+			elseif ExitAlign == 'right' then
+				UiTranslate(-rw,-hprh)
+			end
             
             UiColor(1,1,1,animation)
-            local rw, rh = UiText(name)
-            local size = math.max(rw + PresetPadding.thick, UiWidth() - padx)
-
-            UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
-            bool = UiBlankButton(size, rh + PresetPadding.thick)
-            hover = UiIsMouseInRect(size, rh + PresetPadding.thick)
-        UiPop()
+            UiText(name)
+			
+			local hover = UiIsMouseInRect(prw, prh)
+			if not hover then
+				UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
+			else
+				UiButtonImageBox("ui/common/box-outline-fill-6.png", 6, 6)
+			end
+			
+			local bool = UiBlankButton(prw, prh)
+		UiPop()
 	UiPop()
+			
+	local trw, trh = prw + 6, prh + 6
+	if ExitAlign == 'top' then
+		UiTranslate(0, trh + Spacing)
+	elseif ExitAlign == 'bottom' then
+		UiTranslate(0, -(trh + Spacing))
+	elseif ExitAlign == 'left' then
+		UiTranslate(trw + Spacing, 0)
+	elseif ExitAlign == 'right' then
+		UiTranslate(-(trw + Spacing), 0)
+	end
 
-    UiAlign('center top')
-    UiTranslate(0, rh + pady)
-
-	return {value = bool, hover = hover, rect = {w = rw, h = rh}}
+	return {value = bool, hover = hover, rect = {w = trw, h = trh}}
 end
 
-function Auto.UiEasyText(name, pad, animation)
-	animation = animation and Auto.logistic(animation, 1, -10, 0.5) or 1
-    pad = pad or {'heavy', 'heavy'}
-    padx = PresetPadding[pad[1]]
-    pady = PresetPadding[pad[2]]
+-- function AutoUI.UiEasyText(name, pad, animation)
+-- 	animation = animation and Auto.logistic(animation, 1, -10, 0.5) or 1
+--     pad = pad or {'heavy', 'heavy'}
+--     padx = PresetPadding[pad[1]]
+--     pady = PresetPadding[pad[2]]
     
-	UiPush()
-        UiWordWrap(UiWidth() - padx)
-        UiFont("regular.ttf", 28)
+-- 	UiPush()
+--         UiWordWrap(UiWidth() - padx)
+--         UiFont("regular.ttf", 28)
         
-        UiPush()
-            UiColor(0,0,0,0)
-            local frw, frh = UiText(name)
-            UiTranslate(0,frh/2)
+--         UiPush()
+--             UiColor(0,0,0,0)
+--             local frw, frh = UiText(name)
+--             UiTranslate(0,frh/2)
 
-            UiAlign('center middle')
-            UiColor(1,1,1,animation)
-            local rw, rh = UiText(name)
-            hover = UiIsMouseInRect(rw + PresetPadding.thick, rh + PresetPadding.thick)
-        UiPop()
-	UiPop()
+--             UiAlign('center middle')
+--             UiColor(1,1,1,animation)
+--             local rw, rh = UiText(name)
+--             hover = UiIsMouseInRect(rw + PresetPadding.thick, rh + PresetPadding.thick)
+--         UiPop()
+-- 	UiPop()
 
-    UiAlign('center top')
-    UiTranslate(0, rh + pady)
+--     UiAlign('center top')
+--     UiTranslate(0, rh + pady)
 
-	return {value = bool, hover = hover, rect = {w = rw, h = rh}}
-end
+-- 	return {value = bool, hover = hover, rect = {w = rw, h = rh}}
+-- end
 
--- function Auto.UiEasySlider(pathorvalue, name, default, min, max, lockincrement)
+-- function AutoUI.UiEasySlider(pathorvalue, name, default, min, max, lockincrement)
 -- 	min = min or 0
 -- 	max = max or 1
 -- 	lockincrement = lockincrement or 0
@@ -329,7 +400,7 @@ end
 -- 	return {value = value, hover = hover, slider = hoverslider, rect = {w = rw, h = elapsedheight}}
 -- end
 
--- function Auto.UiEasyTrueFalse (pathorvalue, name, default, enabletext, disabletext)
+-- function AutoUI.UiEasyTrueFalse (pathorvalue, name, default, enabletext, disabletext)
 -- 	enabletext = enabletext or "Enabled"
 -- 	disabletext = disabletext or "Disabled"
 
@@ -383,7 +454,7 @@ end
 -- 	return {value = value, hover = hover, hoveroption = hoveroption, rect = {w = rw, h = rh}}
 -- end
 
-function Auto.UiEasyTooltip(text, position, scale, alpha)
+function AutoUI.UiWorldTooltip(text, position, scale, alpha)
     text = text or "nil"
 	scale = scale or 1
     alpha = alpha or 0.75
@@ -406,4 +477,12 @@ function Auto.UiEasyTooltip(text, position, scale, alpha)
 		UiColor(1,1,1,alpha)
 		UiText(text)
 	UiPop()
+end
+
+function AutoUI.UiMarker()
+	UiPop()
+		UiColor(1, 1, 0.5)
+		UiAlign('center middle')
+		UiImage('ui/common/dot.png')
+	UiPush()
 end
